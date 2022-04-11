bedffdf9f62031bafd761866d5eda736081cf9eb
Ruben Kretek <rubenkretek@outlook.com>
2022-04-11 14:04:12 +0100

    Build: blog archive page

--- src/templates/blog-post-archive.js
+++ src/templates/blog-post-archive.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import { GatsbyImage, StaticImage } from "gatsby-plugin-image"
+
 import parse from "html-react-parser"
 
 import Bio from "../components/bio"
@@ -7,6 +9,8 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 import PageBanner from "../components/PageBanner"
+import { MdLocationPin } from 'react-icons/md';
+
 
 
 const BlogIndex = ({
@@ -28,39 +32,52 @@ const BlogIndex = ({
     )
   }
 
+
+
   return (
     <Layout>
       <PageBanner>
         <h1><span>Cat cafes</span> we have visted</h1>
       </PageBanner>
       <Seo title="All posts" />
-      <Bio />
+      <div className="post-archive">
+        <div className="layout__container">
+          <ol className="post-archive__list">
+            {posts.map(post => {
+              const title = post.title
+              const featuredImage = {
+                data: post.featuredImage?.node?.localFile?.childImageSharp?.gatsbyImageData,
+                alt: post.featuredImage?.node?.alt || ``,
+              }
 
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.title
+              return (
+                <li className="post-archive__list-item" key={post.uri}>
+                  <article
+                    className="post-archive__teaser"
+                    itemScope
+                    itemType="http://schema.org/Article"
+                  >
+                    <div className="post-archive__image">
+                      <GatsbyImage
+                        image={featuredImage.data}
+                        alt={featuredImage.alt}
 
-          return (
-            <li key={post.uri}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.uri} itemProp="url">
-                      <span itemProp="headline">{parse(title)}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.date}</small>
-                </header>
-                <section itemProp="description">{parse(post.excerpt)}</section>
-              </article>
-            </li>
-          )
-        })}
-      </ol>
+                      />
+                    </div>
+                    <div className="post-archive__text">
+                      <h2 className="post-archive__title">{parse(title)}</h2>
+                      <div className="post-archive__location"><MdLocationPin />{post.cafeInfo.location}</div>
+                      <section className="post-archive__description" itemProp="description">{parse(post.excerpt)}</section>
+                      <Link to={post.uri} itemProp="url" className="post-archive__link button">Read more</Link>
+                    </div>
+
+                  </article>
+                </li>
+              )
+            })}
+          </ol>
+        </div>
+      </div>
 
       {previousPagePath && (
         <>
@@ -88,6 +105,25 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         title
         excerpt
+        cafeInfo {
+          location
+        }
+        featuredImage {
+          node {
+            altText
+            localFile {
+              childImageSharp {
+                gatsbyImageData(
+                    quality: 100
+                    placeholder: TRACED_SVG
+                    layout: FULL_WIDTH
+                    aspectRatio: 1.3
+                    transformOptions: {cropFocus: CENTER, fit: COVER}
+                  )
+              }
+            }
+          }
+        }
       }
     }
   }
