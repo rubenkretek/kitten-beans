1149db080fb5a10da27724e145a2571d4b791dfa
Ruben Kretek <rubenkretek@outlook.com>
2022-04-12 17:06:32 +0100

    Build: blog post page

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -15,75 +15,140 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+import PageBanner from "../components/PageBanner"
+
+import { MdLocationPin, MdCalendarToday, MdPublic, MdInfo } from 'react-icons/md';
+
+
 const BlogPostTemplate = ({ data: { previous, next, post } }) => {
   const featuredImage = {
     data: post.featuredImage?.node?.localFile?.childImageSharp?.gatsbyImageData,
     alt: post.featuredImage?.node?.alt || ``,
   }
+  const cafeInfo = post.cafeInfo
 
   return (
     <Layout>
       <Seo title={post.title} description={post.excerpt} />
-
-      <article
-        className="blog-post"
-        itemScope
-        itemType="http://schema.org/Article"
-      >
-        <header>
-          <h1 itemProp="headline">{parse(post.title)}</h1>
-          <p>{post.date}</p>
-          <h2>Location: {post.cafeInfo.location}</h2>
-          <h2>ID: {post.id}</h2>
-
-          {/* if we have a featured image for this post let's display it */}
-          {featuredImage?.data && (
-            <GatsbyImage
-              image={featuredImage.data}
-              alt={featuredImage.alt}
-              style={{ marginBottom: 50 }}
-            />
-          )}
-        </header>
-
-        {!!post.content && (
-          <section itemProp="articleBody">{parse(post.content)}</section>
-        )}
-
-        <hr />
-
-        <footer>
-          <Bio />
-        </footer>
-      </article>
-
-      <nav className="blog-post-nav">
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
+      <PageBanner>
+        <h1>{parse(post.title)}<span><br />Cat cafe</span></h1>
+      </PageBanner>
+      <div className="layout__container layout__container--no-padding-large">
+        <article
+          className="post"
+          itemScope
+          itemType="http://schema.org/Article"
         >
-          <li>
-            {previous && (
-              <Link to={previous.uri} rel="prev">
-                ← {parse(previous.title)}
-              </Link>
-            )}
-          </li>
+          <aside className="post__aside">
+            <h2 className="post__subtitle">About</h2>
+            <div className="post__about-container">
+              <div className="post__about post__about--location">
+                <div className="post__about-icon">
+                  <MdLocationPin />
+                </div>
+                <div className="post__about-label">Location</div>
+                <div className="post__about-info">
+                  <div className="post__about-value">
+                    {cafeInfo.fullAddress}
+                  </div>
+                </div>
+              </div>
 
-          <li>
-            {next && (
-              <Link to={next.uri} rel="next">
-                {parse(next.title)} →
-              </Link>
+              <div className="post__about post__about--date-visited">
+                <div className="post__about-icon">
+                  <MdCalendarToday />
+                </div>
+                <div className="post__about-label">Date Visited</div>
+                <div className="post__about-info">
+                  <div className="post__about-value">
+                    {cafeInfo.dateVisited}
+                  </div>
+                </div>
+              </div>
+
+              <div className="post__about post__about--website">
+                <div className="post__about-icon">
+                  <MdPublic />
+                </div>
+                <div className="post__about-label">Website</div>
+                <div className="post__about-info">
+                  <div className="post__about-value">
+                    {cafeInfo.website}
+                  </div>
+                </div>
+              </div>
+
+              <div className="post__about post__about--features">
+                <div className="post__about-icon">
+                  <MdInfo />
+                </div>
+                <div className="post__about-label">Features</div>
+                <div className="post__about-info">
+                  {cafeInfo.features.map(feature =>
+                    <div className="post__about-value">
+                      {feature.name}
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </aside>
+          <div className="post__main">
+            <header>
+              <h2 className="post__subtitle">{post.cafeInfo.subtitle}</h2>
+              <p className="post__posted-date">Posted on: {post.date}</p>
+            </header>
+
+            {!!post.content && (
+              <section itemProp="articleBody">{parse(post.content)}</section>
             )}
-          </li>
-        </ul>
-      </nav>
+          </div>
+
+          <nav className="post__post-nav">
+            <div className="post__post-nav-title">
+              <h4>Explore other cat cafes</h4>
+            </div>
+            <div className="post__post-nav-menu">
+              <ul className="post__post-nav-list">
+                <li className="post__post-nav-list-item">
+                  {previous && (
+                    <Link to={previous.uri} rel="prev">
+                      ← {parse(previous.title)}
+                    </Link>
+                  )}
+                </li>
+
+                <li className="post__post-nav-list-item">
+                  {next && (
+                    <Link to={next.uri} rel="next">
+                      {parse(next.title)} →
+                    </Link>
+                  )}
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+
+        </article>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </Layout>
   )
 }
@@ -117,8 +182,10 @@ export const pageQuery = graphql`
         }
       }
       cafeInfo {
+        subtitle
         dateVisited
         location
+        fullAddress
         website
         features {
           id
